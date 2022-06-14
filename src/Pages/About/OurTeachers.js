d91ac144b8c3db34d9d4867eba3ecab5d7eb35e7
zamanmayed1 <zamanmayed02@gmail.com>
2022-06-14 23:45:25 +0600

    Our Teachers Part added by Mayed

--- src/Pages/About/OurTeachers.js
+++ src/Pages/About/OurTeachers.js
@@ -1,9 +1,103 @@
 import React from 'react';
-
+import { BsFacebook, BsYoutube } from 'react-icons/bs'
+import { AiFillInstagram } from 'react-icons/ai'
 const OurTeachers = () => {
     return (
-        <div>
-            <h1>Our Teachers</h1>
+        <div className='lg:max-w-7xl md-w-full  mx-auto px-4 mt-16  md:px-16 '>
+            <div className='md:flex gap-10  my-4'>
+                <div className=' md:w-2/4'>
+                    <h1 className='md:text-3xl text-2xl font-bold'>আমাদের দক্ষ প্রশিক্ষক মন্ডলি</h1>
+                </div>
+                <div className=' md:w-3/4'>
+                    <h1 className='md:text-xl font-light text-sm'>মুক্ত কৌশলগত থিম এলাকার মাধ্যমে বৈপ্লবিক উদ্ভাবনকে স্বতন্ত্রভাবে পুনরায় সংজ্ঞায়িত করুন।
+                        উচ্চ-দক্ষ আর্কিটেকচারের পরিবর্তে উচ্চ-প্রদান পরীক্ষার পদ্ধতিগুলিকে ফসফ্লুরোসেন্টলি ই-সক্ষম করে।</h1>
+                </div>
+
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 gap-8  md:px-0  '>
+
+                {/* Teacher Card Start */}
+                <div className='p-2 cs-border-shadow rounded-md w-full md:max-w-[280px]  '>
+                    <div className='h-[150px] mx-auto  w-[150px] rounded-full overflow-hidden'>
+                        <img className='w-full h-full' src="https://a.storyblok.com/f/114723/802x1010/a3039fe14d/shalon-teacher-online.JPG/m/0x330" alt="" />
+                    </div>
+                    <div className='w-full h-[100px] text-center mt-2'>
+                        <h1 className='text-xl font-bold '>Md Moshiur Rahamn </h1>
+                        <h1 className='text-sm font-bold text-[#515FCE] '>Web Developer </h1>
+                        <div className='flex justify-center mt-4'>
+                            <BsFacebook className='text-xl mx-2 hover:text-[#515FCE]'></BsFacebook>
+                            <BsYoutube className='text-xl mx-2 hover:text-[#515FCE]'></BsYoutube>
+                            <AiFillInstagram className='text-xl mx-2 hover:text-[#515FCE]'></AiFillInstagram>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                {/* Teacher Card End----------------- */}
+                {/* Teacher Card Start */}
+                <div className='p-2 cs-border-shadow rounded-md w-full md:max-w-[280px]  '>
+                    <div className='h-[150px] mx-auto  w-[150px] rounded-full overflow-hidden'>
+                        <img className='w-full h-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzyDJ1LbPIchk5iU_I7dvAR779j95KUZ31kw&usqp=CAU" alt="" />
+                    </div>
+                    <div className='w-full h-[100px] text-center mt-2'>
+                        <h1 className='text-xl font-bold '>Md Moshiur Rahamn </h1>
+                        <h1 className='text-sm font-bold text-[#515FCE] '>Web Developer </h1>
+                        <div className='flex justify-center mt-4'>
+                            <BsFacebook className='text-xl mx-2 hover:text-[#515FCE]'></BsFacebook>
+                            <BsYoutube className='text-xl mx-2 hover:text-[#515FCE]'></BsYoutube>
+                            <AiFillInstagram className='text-xl mx-2 hover:text-[#515FCE]'></AiFillInstagram>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                {/* Teacher Card End----------------- */}
+                {/* Teacher Card Start */}
+                <div className='p-2 cs-border-shadow rounded-md w-full md:max-w-[280px]  '>
+                    <div className='h-[150px] mx-auto  w-[150px] rounded-full overflow-hidden'>
+                        <img className='w-full h-full' src="https://miro.medium.com/focal/294/294/45/30/1*H3LhaRn9lcqRVylFt9PAIw.jpeg" alt="" />
+                    </div>
+                    <div className='w-full h-[100px] text-center mt-2'>
+                        <h1 className='text-xl font-bold '>Md Moshiur Rahamn </h1>
+                        <h1 className='text-sm font-bold text-[#515FCE] '>Web Developer </h1>
+                        <div className='flex justify-center mt-4'>
+                            <BsFacebook className='text-xl mx-2 hover:text-[#515FCE]'></BsFacebook>
+                            <BsYoutube className='text-xl mx-2 hover:text-[#515FCE]'></BsYoutube>
+                            <AiFillInstagram className='text-xl mx-2 hover:text-[#515FCE]'></AiFillInstagram>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                {/* Teacher Card End----------------- */}
+                {/* Teacher Card Start */}
+                <div className='p-2 cs-border-shadow rounded-md w-full md:max-w-[280px]  '>
+                    <div className='h-[150px] mx-auto  w-[150px] rounded-full overflow-hidden'>
+                        <img className='w-full h-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXPOfQvgkb_knJHkFtr5VfCTuFou6WUiSCOAEfHqD8ybrkF5Wq4oujlkxuds5kIbjd7-s&usqp=CAU" alt="" />
+                    </div>
+                    <div className='w-full h-[100px] text-center mt-2'>
+                        <h1 className='text-xl font-bold '>Md Moshiur Rahamn </h1>
+                        <h1 className='text-sm font-bold text-[#515FCE] '>Web Developer </h1>
+                        <div className='flex justify-center mt-4'>
+                            <BsFacebook className='text-xl mx-2 hover:text-[#515FCE]'></BsFacebook>
+                            <BsYoutube className='text-xl mx-2 hover:text-[#515FCE]'></BsYoutube>
+                            <AiFillInstagram className='text-xl mx-2 hover:text-[#515FCE]'></AiFillInstagram>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                {/* Teacher Card End----------------- */}
+
+            </div>
         </div>
     );
 };
--- src/Pages/Home/LatestBlogs.js
+++ src/Pages/Home/LatestBlogs.js
@@ -9,7 +9,7 @@ const LatestBlogs = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center mt-10'>
 
                 <div className='border shadow rounded-md max-w-sm w-full'>
-                    <img className='w-full h-[230px]' src={card1} alt="" />
+                    <img className='w-full h-[230px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7cCNiMpYATgXHdPUO3irVRUxh16Pxwo99nhqJ1rxabqx0oFvc3fZ-3CC3E5y5oz-vD5c&usqp=CAU' alt="" />
                     <div className='flex mx-3 mt-4 lg:font-medium lg:gap-4 gap-2 mb-4'>
                         <p>20-04-2022</p>
                         <div className='border border-[#CEC051]'></div>
@@ -22,7 +22,7 @@ const LatestBlogs = () => {
                     <button className='buttonmodal btn1 mx-3 mt-4 mb-4'>পড়ুন</button>
                 </div>
                 <div className='border shadow rounded max-w-sm w-full'>
-                    <img className='w-full h-[230px]' src={card1} alt="" />
+                    <img className='w-full h-[230px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGvhCzeeaHeWa3PXDG3D3_FXP-HWiGWOx0BFPykSMmyTxTOMEfVpPJSSF7AxEqZCM-gAw&usqp=CAU' alt="" />
                     <div className='flex mx-3 mt-4 lg:font-medium lg:gap-4 gap-2 mb-4'>
                         <p>20-04-2022</p>
                         <div className='border border-[#CEC051]'></div>
@@ -35,7 +35,7 @@ const LatestBlogs = () => {
                     <button className='buttonmodal btn1 mx-3 mt-4 mb-4'>পড়ুন</button>
                 </div>
                 <div className='border shadow rounded max-w-sm w-full'>
-                    <img className='w-full h-[230px]' src={card1} alt="" />
+                    <img className='w-full h-[230px]' src='https://leverageedu.com/blog/wp-content/uploads/2019/11/Primary-Teaching-Courses.jpg' alt="" />
                     <div className='flex mx-3 mt-4 lg:font-medium lg:gap-4 gap-2 mb-4 w-full'>
                         <p>20-04-2022</p>
                         <div className='border border-[#CEC051]'></div>
