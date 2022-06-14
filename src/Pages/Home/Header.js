9ee4e4de61c2e1f986d644d265275fa0559aa408
zamanmayed1 <zamanmayed02@gmail.com>
2022-06-14 13:57:16 +0600

    Home Catagory by zaman

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -2,25 +2,30 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='bg-[#FDFCF6] px-6 md:px-14 lg:px-24 h-[70vh]'>
-           <div className='lg:flex lg:justify-between items-center'>
+        <div className='bg-[#FDFCF6] '>
+            <div className='px-6 md:px-14 lg:max-w-7xl md-w-full mx-auto min-h-[60vh] h-auto'>
+                <div className='flex flex-wrap'>
+                    {/* <div className='lg:flex  md:flex-row lg:justify-between items-center'> */}
 
-           <div className=''>
-                <h1 className='text-2xl md:text-3xl lg:text-4xl'>সেরা <span className='lg:text-4xl text-orange-200'>ই-লার্নিং</span> প্ল্যাটফর্ম </h1>
-                <h1 className='text-2xl md:text-3xl mt-1 lg:text-4xl'>  বাংলাদেশে</h1>
-                <p className='mt-4'>
-                    মুক্ত কৌশলগত থিম এলাকার মাধ্যমে বৈপ্লবিক উদ্ভাবনকে স্বতন্ত্রভাবে পুনরায় সংজ্ঞায়িত
-                    করুন।উচ্চ-দক্ষ আর্কিটেকচারের পরিবর্তে উচ্চ-প্রদান পরীক্ষার পদ্ধতিগুলিকে
-                    ফসফ্লুরোসেন্টলি ই-সক্ষম করে।
-                </p>
-                  <button className=' bg-[#515FCE] px-8 py-3 mt-8 rounded text-white'>শুরু করি</button>
+                    <div className='md:w-2/4   md:text-left flex items-center'>
+                        <div>
+                            <h1 className='text-2xl md:text-3xl lg:text-5xl font-bold'>সেরা <span className='lg:text-5xl text-[#CEC051] font-bold'>ই-লার্নিং</span> প্ল্যাটফর্ম </h1>
+                            <h1 className='text-2xl md:text-3xl mt-1 lg:text-5xl font-bold'>  বাংলাদেশে</h1>
+                            <p className='mt-4'>
+                                মুক্ত কৌশলগত থিম এলাকার মাধ্যমে বৈপ্লবিক উদ্ভাবনকে স্বতন্ত্রভাবে পুনরায় সংজ্ঞায়িত
+                                করুন।উচ্চ-দক্ষ আর্কিটেকচারের পরিবর্তে উচ্চ-প্রদান পরীক্ষার পদ্ধতিগুলিকে
+                                ফসফ্লুরোসেন্টলি ই-সক্ষম করে।
+                            </p>
+                            <button className=' bg-[#515FCE] px-8 py-3 mt-8 rounded text-white'>শুরু করি</button>
+                        </div>
+                    </div>
+
+                    <div className=' md:w-2/4 w-full  justify-center items-center'>
+                        <img className='md:w-3/4  mx-auto block' src="https://i.ibb.co/qYz3Prh/Group-5200.png" alt="" />
+                    </div>
+
+                </div>
             </div>
-
-             <div className='mt-8'>
-                <img src="https://cdn.dribbble.com/users/1894420/screenshots/14032021/programming_01.gif" alt="" />
-             </div>
-
-           </div>
         </div>
     );
 };
--- src/Pages/Home/HomeCatagory.js
+++ src/Pages/Home/HomeCatagory.js
@@ -1,10 +1,123 @@
-import React from 'react';
+
+import React from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
+import './HomeCatagory.css'
+
+
+
+
+
 
 const HomeCatagory = () => {
+
+
+
+    const leftscroll = () => {
+
+        const div = document.getElementById('mainscroll')
+        div.scrollLeft -= 70
+
+    }
+    const rightscroll = () => {
+
+        const div = document.getElementById('mainscroll')
+        div.scrollLeft += 70
+
+    }
     return (
-        <div>
-            
-        </div>
+        <div className='lg:max-w-7xl md-w-full  mx-auto px-4  md:px-16 '>
+            <div>
+                <h1 className='md:text-4xl text-2xl font-bold my-2'>বৈশিষ্ট্য কোর্স</h1>
+
+                <div className="flex ">
+                    <div onClick={leftscroll} className="h-[35px] w-[35px]">
+                        <BsArrowLeftCircleFill
+                            className="w-full h-full block text-[#CEC051]" />
+                    </div>
+                    <div className="flex w-full overflow-x-scroll   homecatagory " id="mainscroll">
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>1</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>2</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>3</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+                        <div className='px-4 py-2 inline-block mx-2 hover:bg-[#B3EAFF] hover:font-bold rounded-md bg-transparent border boder-[#41CCFF]'>
+                            <h3>UX/UI</h3>
+                        </div>
+
+                        <div>
+
+
+                        </div>
+
+                    </div>
+                    <div onClick={rightscroll} className="h-[35px] w-[35px] flex justify-center items-center">
+                        <BsArrowRightCircleFill className="w-full h-full block text-[#CEC051]" />
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+            </div>
+
+        </div >
     );
 };
 
