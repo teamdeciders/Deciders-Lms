d0686987b555e0f15d0efed10823eb62a136b265
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-06-15 16:17:45 +0600

    navbar done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,19 +8,19 @@ const Navbar = () => {
     const [open, setOpen] = useState(false)
 
     return (
-        <nav className='md:px-24 px-6'>
+        <nav className='md:px-24 px-4 bg-[#FDFCF6] sticky top-0 z-10'>
             <div className='w-full sticky'>
-                <div className='md:flex items-center justify-between bg-white py-4'>
+                <div className='md:flex items-center justify-between  py-4'>
                     <div className='font-bold text-2xl cursor-pointer flex items-start 
       text-gray-800'>
                         <Link to={'/'} >ABC</Link>
                     </div>
 
-                    <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-4 cursor-pointer md:hidden'>
-                        {open ? <GrClose /> : <AiOutlineMenu />}
+                    <div onClick={() => setOpen(!open)} className='text-3xl absolute right-2 top-4 cursor-pointer md:hidden'>
+                        {open ? <GrClose className='h-5 w-5' /> : <AiOutlineMenu className='h-6 w-6' />}
                     </div>
 
-                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-14 ' : 'top-[-370px]'}`}>
+                    <ul onClick={() => setOpen(!open)} className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static lg:bg-[#FDFCF6] md:bg-[#FDFCF6] bg-[#FDFCF6]  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-14 ' : 'top-[-370px]'}`}>
                         <li className='md:ml-8 text-xl md:my-0 my-7'>
                             <Link to={'/'} className='text-gray-800 hover:text-gray-400 duration-500'>Home</Link>
                         </li>
