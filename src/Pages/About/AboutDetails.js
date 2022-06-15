4ce3a95c47a33ebae58698ce4fea2917f2020a4f
SADEK HOSSAIN <shadeks16@gmail.com>
2022-06-15 12:48:21 +0600

    about details part done by sadek hossain

--- src/Pages/About/AboutDetails.js
+++ src/Pages/About/AboutDetails.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const AboutDetails = () => {
-    return (
-        <div>
-            <h1>I am About Details</h1>
+  return (
+    <div className="lg:max-w-7xl md-w-full mx-auto my-28 px-6  md:px-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+        <div className="rounded-xl p-5 h-[318px] w-full max-w-sm mx-auto border border-[#D9F5FF] hover:border-4">
+          <div className="text-4xl my-10 ">
+            <h2 className="text-xl font-bold hover:text-[#515FCE]">
+              আমরা কারা?{" "}
+            </h2>
+          </div>
+          <div>
+            <p className="mt-3 text-justify">
+              {" "}
+              মুক্ত কৌশলগত থিম এলাকার মাধ্যমে বৈপ্লবিক উদ্ভাবনকে স্বতন্ত্রভাবে
+              পুনরায় সংজ্ঞায়িত করুন। উচ্চ-দক্ষ আর্কিটেকচারের পরিবর্তে
+              উচ্চ-প্রদান পরীক্ষার পদ্ধতিগুলিকে ফসফ্লুরোসেন্টলি ই-সক্ষম করে।
+            </p>
+          </div>
         </div>
-    );
+
+        <div className="rounded-xl p-5 h-[318px] w-full max-w-sm mx-auto border border-[#D9F5FF] hover:border-4">
+          <div className="text-4xl my-10 ">
+            <h2 className="text-xl font-bold hover:text-[#515FCE]">
+              আমরা কি করি?{" "}
+            </h2>
+          </div>
+          <div>
+            <p className="mt-3 text-justify">
+              {" "}
+              মুক্ত কৌশলগত থিম এলাকার মাধ্যমে বৈপ্লবিক উদ্ভাবনকে স্বতন্ত্রভাবে
+              পুনরায় সংজ্ঞায়িত করুন। উচ্চ-দক্ষ আর্কিটেকচারের পরিবর্তে
+              উচ্চ-প্রদান পরীক্ষার পদ্ধতিগুলিকে ফসফ্লুরোসেন্টলি ই-সক্ষম করে।
+            </p>
+          </div>
+        </div>
+
+        <div className="rounded-xl p-5 h-[318px] w-full max-w-sm mx-auto border border-[#D9F5FF] hover:border-4">
+          <div className="text-4xl my-10 ">
+            <h2 className="text-xl font-bold hover:text-[#515FCE]">
+              কেন আমাদের নির্বাচন করেছে?{" "}
+            </h2>
+          </div>
+          <div>
+            <p className="mt-3 text-justify">
+              {" "}
+              মুক্ত কৌশলগত থিম এলাকার মাধ্যমে বৈপ্লবিক উদ্ভাবনকে স্বতন্ত্রভাবে
+              পুনরায় সংজ্ঞায়িত করুন। উচ্চ-দক্ষ আর্কিটেকচারের পরিবর্তে
+              উচ্চ-প্রদান পরীক্ষার পদ্ধতিগুলিকে ফসফ্লুরোসেন্টলি ই-সক্ষম করে।
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutDetails;
