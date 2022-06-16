6fc1f212f67acc1e3a596a6ee06d785fae9476ac
zamanmayed1 <zamanmayed02@gmail.com>
2022-06-16 14:40:37 +0600

    Check

--- src/Pages/SingleCourse/SingleCourse.js
+++ src/Pages/SingleCourse/SingleCourse.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const SingleCourse = () => {
     return (
-        <div>
-            <p>This is Single Course Page!!!</p>
-        </div>
+      <div>
+         <div className=' h-[30vh]  bg-[#FDFCF6] w-full'>
+                <div className='lg:max-w-7xl md-w-full  mx-auto px-4  md:px-16 '>
+                    <h1 className='text-4xl font-bold text-center pt-20'>Course Details</h1>
+                </div>
+            </div>
+      </div>
     );
 };
 
