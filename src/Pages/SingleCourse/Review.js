ad299c553ea6184a215ed9190844ac2667f83baf
zamanmayed1 <zamanmayed02@gmail.com>
2022-06-17 00:22:49 +0600

    Single page review , instractor page

--- src/Pages/SingleCourse/Review.js
+++ src/Pages/SingleCourse/Review.js
@@ -2,8 +2,299 @@ import React from 'react';
 
 const Review = () => {
     return (
-        <div>
-            review
+        <div className='h-[80vh] overflow-y-scroll review'>
+            <div className='grid md:grid-cols-2 gap-5 rounded-md'>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/1nNrJJx/Kids-Teacher-04.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/qrK4XbD/Kids-Teacher-03.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/XDQWBfh/images.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://s.udemycdn.com/home/non-student-cta/ub-1x-v3.jpg" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://s.udemycdn.com/home/non-student-cta/instructor-1x-v3.jpg" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://s.udemycdn.com/teaching/instructors/en/frank-1x-v2.jpg" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+                <div className='shadow p-4 '>
+                    <div className='flex gap-4'>
+                        <img className='w-16 h-16 ring-2 rounded-full' src="https://i.ibb.co/tqddb5s/Kids-Teacher-02.png" alt="" />
+                        <div>
+                            <h2 className='text-xl'>কামরুল হক</h2>
+                            <h2 className='text-sm text-gray-600'>@kamrul528</h2>
+                            <h2 className='text-sm text-gray-600'>2 Course Done</h2>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <h2 className='text-sm mt-2 text-gray-600'>
+                            স্যার এর কোর্স টি করে অনেক ভালো লাগলো । এত সুন্দর করে কোর্সটি সাজানো হয়েছে যে , নতুন দের জন্য দেশ সেরা কোর্স । অনেক অনেক ধন্যবাদ স্যার কে এত সুন্দর কোর্স উপহার দেয়ার জন্য ।
+                        </h2>
+                    </div>
+
+                </div>
+
+
+            </div>
         </div>
     );
 };
