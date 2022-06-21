bfff97c8f8f2a634b53b9cecff94e362e12642d4
zamanmayed1 <zamanmayed02@gmail.com>
2022-06-21 22:46:21 +0600

    Dasboard Setup Success

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,11 +1,65 @@
 import React from 'react';
-
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineShoppingCart ,AiOutlineLogout} from 'react-icons/ai'
+import { BiUserCircle } from 'react-icons/bi'
+import { MdOutlineHistoryEdu } from 'react-icons/md'
+import CustomLink from './CustomLink';
 const Dashboard = () => {
-    return (
-        <div>
-            dashboard
+  let name = 'Kamruzzaman Mayed'
+
+  return (
+    <div className=''>
+      <div className='  bg-[#FDFCF6] w-full '>
+        <div className='lg:max-w-7xl md-w-full  mx-auto px-4  md:px-12 '>
+          <div className=" mx-auto w-[375px] py-8">
+            <div className="w-[100px] h-[100px] rounded-full mx-auto bg-blue-50 border-2 border-black overflow-hidden ">
+              <img className='w-full h-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwejmeEUc09aMbmR3vgKj9udJaNn9imJO0uPD1ksSgNUyL2Vu1-2rj3CYk493Ku0BPGbY&usqp=CAU" alt="" />
+            </div>
+            <div className='text-center'>
+              <h2 className='text-2xl font-bold my-2'>{name}</h2>
+            </div>
+            <div className=' p-2 flex gap-6 justify-center'>
+              <div className='text-center' >
+                <h2 className='text-3xl font-bold'>3</h2>
+                <h2 className=' font-bold text-gray-600'>Courses</h2>
+              </div>
+              <div className='text-center' >
+                <h2 className='text-3xl font-bold'>2</h2>
+                <h2 className='font-bold text-gray-600'>Completed</h2>
+              </div>
+              <div className='text-center' >
+                <h2 className='text-3xl font-bold'>0</h2>
+                <h2 className=' font-bold text-gray-600'>Certificates</h2>
+              </div>
+
+            </div>
+
+
+          </div>
+
         </div>
-    );
+      </div>
+      <div className="w-full mt-8">
+        <div className='lg:max-w-7xl md-w-full  mx-auto md:flex gap-2'>
+          <div className='md:w-[20%]  min-h-max rounded-sm shadow-md p-2'>
+
+
+            <CustomLink className="hover:bg-[#FC8B06]  " to={''}><AiOutlineHome className='inline-block text-xl' /> Home</CustomLink>
+            <CustomLink className="hover:bg-[#FC8B06]" to={'myprofile'}><BiUserCircle className='inline-block text-xl' />My Profile</CustomLink>
+            <CustomLink className="hover:bg-[#FC8B06]" to={'mycourse'}><MdOutlineHistoryEdu className='inline-block text-xl' />My Courses</CustomLink>
+            <CustomLink className="hover:bg-[#FC8B06]" to={'mycart'}> <AiOutlineShoppingCart className='inline-block text-xl' /> My Cart</CustomLink>
+           
+          </div>
+          <div className='md:w-[80%] rounded-sm  shadow-md p-2'>
+            {/* Out let Here */}
+
+            <Outlet />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,36 +5,36 @@ import { FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-     const [showPassword , setShowPassword] = useState(false)
-    return (
-       <div className='lg:flex justify-around mt-12'>
+   const [showPassword, setShowPassword] = useState(false)
+   return (
+      <div className='lg:flex justify-center mt-12'>
          <div className='hidden lg:block'>
             <img src="https://lh3.googleusercontent.com/BLPgeHnmm_jXD9odvEPr6y3-9tBKwfgOXEk7ysvnIPcCbCJ4WtCTfHnvgzqV-fHHrSJhx-ixjfwa2nVpy6TnnX28ErUNU3UtTg=w911" alt="" />
          </div>
-         <div className='md:w-[500px] w-full mx-auto shadow-xl p-8 rounded'>
+         <div className='md:w-[500px] w-full  shadow-xl p-8 rounded'>
             <p className='text-xl font-semibold mb-4'>Log In to Your Deciders Account!</p>
-             <form>
-                <input type="email" className='py-3 px-3 border border-[#515FCE] rounded w-full mb-4' placeholder='Your Email Address'/>
-                  <div className='relative'>
-                  <input type={showPassword? 'text': 'password'} className='py-3 px-3 border border-[#515FCE] rounded w-full' placeholder='Your Password'/>
-                    
-                 <AiOutlineEyeInvisible onClick={()=>setShowPassword(!showPassword)} className='absolute top-4 right-4' ></AiOutlineEyeInvisible>          
-                  </div>
-              
-                <div className='flex justify-end mt-3'><p className='font-medium text-[#515FCE]'>Forget Password</p></div>
-                 <button className=' bg-[#515FCE] w-full py-3 rounded text-white mt-4'>Login</button>
-                  <div className='md:flex justify-evenly gap-4 mt-4'>
+            <form>
+               <input type="email" className='py-3 px-3 border border-[#515FCE] rounded w-full mb-4' placeholder='Your Email Address' />
+               <div className='relative'>
+                  <input type={showPassword ? 'text' : 'password'} className='py-3 px-3 border border-[#515FCE] rounded w-full' placeholder='Your Password' />
+
+                  <AiOutlineEyeInvisible onClick={() => setShowPassword(!showPassword)} className='absolute top-4 right-4' ></AiOutlineEyeInvisible>
+               </div>
+
+               <div className='flex justify-end mt-3'><p className='font-medium text-[#515FCE]'>Forget Password</p></div>
+               <button className=' bg-[#515FCE] w-full py-3 rounded text-white mt-4'>Login</button>
+               <div className='md:flex justify-evenly gap-4 mt-4'>
                   <button className=' bg-[#FC8B06] w-full py-3 px-1 rounded mt-4'><span className='flex items-center gap-2 justify-center'><FaGoogle></FaGoogle> Continue With Google</span></button>
                   <button className=' bg-[#515FCE] w-full py-3 px-1 text-white rounded mt-4'><span className='flex items-center gap-2 justify-center'><BsFacebook></BsFacebook> Continue With Facebook</span></button>
-                  </div>
-                  <p className='font-medium text-center mt-5'>New To Dciders Lms? <Link to='/singup'><span className='text-[#515FCE]'>Create an Account</span></Link></p>
-             </form>
-        </div>
-        
-       </div>
+               </div>
+               <p className='font-medium text-center mt-5'>New To Dciders Lms? <Link to='/singup'><span className='text-[#515FCE]'>Create an Account</span></Link></p>
+            </form>
+         </div>
+
+      </div>
 
 
-    );
+   );
 };
 
 export default Login;
