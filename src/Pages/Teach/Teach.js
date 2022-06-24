5bb5eb2b203a89617fc86d865088022bd44c5011
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-25 01:37:52 +0600

    instrucor form submission reverse

--- src/Pages/Teach/Teach.js
+++ src/Pages/Teach/Teach.js
@@ -30,6 +30,25 @@ const Teach = () => {
 
             <div className='lg:max-w-7xl w-full  mx-auto px-4  md:px-16 mt-16'>
                 <div className='grid grid-cols-1 md:grid-cols-3 md:gap-x-10 gap-y-8'>
+
+
+                    <div className='col-span-2 border p-7 rounded-lg shadow-lg'>
+                        <h1 className='text-3xl font-semibold mb-12'>Instructor form Submission</h1>
+                        <div>
+                            <form>
+                                <div className='grid grid-cols-1 gap-7'>
+                                    <input type="text" name="name" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Name" />
+                                    <input type="email" name="email" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Email" />
+                                    <input type="number" name="phone" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Phone" />
+                                    <input type="text" name="website" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="LinkdIne Link" />
+                                    <input type="text" name="website" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Area of Expertise" />
+                                </div>
+                                <input className='bg-[#515FCE] p-2 rounded-md text-white mt-9 cursor-pointer' type="submit" value="Submit Now" />
+                            </form>
+                        </div>
+                    </div>
+
+
                     <div className='md:h-1/2 border p-5 shadow-lg rounded-md bg-[#0B2519] text-white'>
                         <h1 className='text-3xl font-semibold text-center'>Need Any Help?</h1>
                         <div className='mt-8'>
@@ -52,21 +71,8 @@ const Teach = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='col-span-2 border p-7 rounded-lg shadow-lg'>
-                        <h1 className='text-3xl font-semibold mb-12'>Instructor form Submission</h1>
-                        <div>
-                            <form>
-                                <div className='grid grid-cols-1 gap-7'>
-                                    <input type="text" name="name" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Name" />
-                                    <input type="email" name="email" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Email" />
-                                    <input type="number" name="phone" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Phone" />
-                                    <input type="text" name="website" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="LinkdIne Link" />
-                                    <input type="text" name="website" className="mt-1 px-3 py-2 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Area of Expertise" />
-                                </div>
-                                <input className='bg-[#515FCE] p-2 rounded-md text-white mt-9 cursor-pointer' type="submit" value="Submit Now" />
-                            </form>
-                        </div>
-                    </div>
+
+                    
                 </div>
             </div>
         </section>
