ac8c008411afcce6b4f1ff7fc0ec46ba6ffe63a6
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-07-02 16:35:37 +0600

    dashboard problem solved

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
 const Loading = () => {
+
+
     return (
         <div>
             <h1 className='text-center text-red-400'>Loading.........</h1>
+
+
+
         </div>
     );
 };
---
+++ src/Pages/SentReview/SentReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SentReview = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SentReview;
